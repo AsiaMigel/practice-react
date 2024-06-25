f548ec97d10c0406e73fa778fa24d0a689509836
Asia <royfinaa@gmail.com>
2024-06-25 12:46:28 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
 import "../App.css";
-import Form from "./Form/Form";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  const submit = (e) => {
-    console.log(e);
-  };
-  return <Form submit={submit} />;
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        const response = await axios.get("<https://hn.algolia.com/api/v1/search?query=react>");
+        setArticles(response.data.hits);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchArticles();
+  }, []);
+  return (
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
+      {error && (<p>Whoops, something went wrong! Please try reloading this page!</p>)}
+      {articles.length > 0 && <ArticleList items={articles}/>}
+    </div>
+  )
 }
-
 export default App;
  
